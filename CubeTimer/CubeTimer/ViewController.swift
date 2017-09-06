495bc8b980f52a8f3496d1d79e8b24dc3074dd29
Scott Pogatetz <apcs2@p-162mba-29.d214.org>
2017-09-06 09:57:32 -0500

    Started collectionViewCell class

--- CubeTimer/CubeTimer/ViewController.swift
+++ CubeTimer/CubeTimer/ViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+
+class navigationCollectionCell : UICollectionViewCell{
+    var image : UIImage
+    var name : String
+    
+    init(name : String, image : UIImage, frame : CGRect) {
+        self.image = image
+        self.name = name
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
