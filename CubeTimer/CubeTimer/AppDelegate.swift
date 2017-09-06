5fffb0690ffaafdfde5ba671842f26234107685f
Scott Pogatetz <apcs2@p-162mba-29.d214.org>
2017-09-06 09:52:02 -0500

    Renamed view controller class

--- CubeTimer/CubeTimer/AppDelegate.swift
+++ CubeTimer/CubeTimer/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        let mainViewController = ViewController()
+        let mainViewController = MainViewController()
         mainViewController.view.backgroundColor = UIColor.white
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
