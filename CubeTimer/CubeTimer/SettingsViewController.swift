364be9676c7601b8314fa4a066b105733433f1d5
Scott Pogatetz <apcs2@p-162mba-29.d214.org>
2017-10-10 09:37:03 -0500

    Started SettingsViewController

---
+++ CubeTimer/CubeTimer/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  CubeTimer
+//
+//  Created by apcs2 on 10/10/17.
+//  Copyright © 2017 apcs2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
